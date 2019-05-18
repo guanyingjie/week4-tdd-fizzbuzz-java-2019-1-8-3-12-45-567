3a6083734fd11d2c037dfc1e9df3069f93c7f1c7
guanyingjie <39229309+guanyingjie@users.noreply.github.com>
2019-05-18 15:18:30 +0800

    finish fizzbuzz function

--- src/main/java/com/thoughtworks/tdd/FizzBuzz.java
+++ src/main/java/com/thoughtworks/tdd/FizzBuzz.java
@@ -1,4 +1,21 @@
 package com.thoughtworks.tdd;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class FizzBuzz {
+    private List<SpecialNumber>specialNumbers = new ArrayList<>();
+    public FizzBuzz(SpecialNumber... special){
+        specialNumbers.addAll(Arrays.asList(special));
+    }
+    public void count(int number){
+        StringBuilder result = new StringBuilder();
+        specialNumbers.forEach(e ->{
+            if(e.isSpecialNumber(number)){
+                result.append(e.getSpecialName());
+            }
+        });
+        System.out.println(result.toString().isEmpty()?number:result.toString());
+    }
 }
--- src/main/java/com/thoughtworks/tdd/main.java
+++ src/main/java/com/thoughtworks/tdd/main.java
@@ -1,4 +1,11 @@
 package com.thoughtworks.tdd;
 
 public class main {
+    public static void main(String[] args){
+        FizzBuzz fizzBuzz = new FizzBuzz(new Fizz(),new Buzz(),new Whizz());
+        for(int i=0;i<120;i++)
+        {
+            fizzBuzz.count(i+1);
+        }
+    }
 }
